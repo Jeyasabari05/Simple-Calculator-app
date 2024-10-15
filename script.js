170d1ba5f180f99ddc4b78bbd343b427558706e2
Jeyasabari S <165381370+Jeyasabari05@users.noreply.github.com>
2024-10-15 21:49:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const display = document.getElementById("inputbox");
+const buttons = document.querySelectorAll("button");
+
+const buttonArray = Array.from(buttons);
+
+let string = "";
+
+buttonArray.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.target.innerHTML === "=") {
+      string = eval(string);
+      display.value = string;
+    } else if (e.target.innerHTML === "DEL") {
+      string = string.slice(0, -1);
+      display.value = string;
+    } else if (e.target.innerHTML === "AC") {
+      string = ""
+      display.value = string;
+    } else {
+      string += e.target.innerHTML;
+      display.value = string;
+    }
+  })
+});
